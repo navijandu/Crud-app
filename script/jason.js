e53ed03912e0903b6094bd65c1d28d36d917d188
Ashmeet Singh <ashmeetsingh2010@gmail.com>
2021-01-23 18:46:45 -0500

    first

---
+++ script/jason.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+
+    // Parse any JSON previously stored in allEntries
+    var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+    if (existingEntries == null) {
+
+    } else {
+        var showTable = existingEntries.length;
+
+        for (i = 0; i < showTable; i++) {
+            $("#sNo").append("<tr><td>" + i + "</td></tr>");
+            $("#resultF").append("<tr><td>" + existingEntries[i].title + "</td></tr>");
+            $("#resultL").append("<tr><td>" + existingEntries[i].text + "</td></tr>");
+            $("#age").append("<tr><td>" + existingEntries[i].Age + "</td></tr>");
+            $("#gender").append("<tr><td>" + existingEntries[i].gender + "</td></tr>");
+
+            $("#dlt").append("<tr id=" + i + "><td><Button id='dltButton' onClick='delet(this)'>Delet</button><Button id='editButton' onClick='edit(this)'>Edit</button></td></tr>");
+            //$("#resultF").append("<tr><td>" + existingEntries[i].title + "</td><td>" + existingEntries[i].text + "</td><td>" + age + "</td></tr>");
+        }
+    }
+    // click button to push the new entry to database
+    $("#submit").click(function () {
+        if ($(this).hasClass("edit")) {
+            var d = $(this).attr("editid")
+            var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+            existingEntries[d].title = $("#fname").val()
+            existingEntries[d].text = $("#lname").val()
+            existingEntries[d].Age = $("#Age").val()
+            existingEntries[d].gender = $("input:radio[name=gender]:checked").val()
+            localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+            location.reload();
+
+        } else {
+            if (existingEntries == null) existingEntries = [];
+            var fname = $("#fname").val()
+            var lname = $("#lname").val()
+            var age = $("#Age").val()
+            var sex = $("input:radio[name=gender]:checked").val()
+
+            var entry = {
+                "title": fname,
+                "text": lname,
+                "Age": age,
+                "gender": sex
+            };
+
+            //   localStorage.setItem("entry", JSON.stringify(entry));
+            // Save allEntries back to local storage
+            existingEntries.push(entry);
+            localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+            /* appending to table */
+            //    $("#resultF").append("<tr><td>'+ sNo +'</td><td>" + fname + "</td><td>" + lname + "</td><td>" + age + "</td></tr>");
+            location.reload();
+        }
+    });
+
+
+
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.ml3');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({
+            loop: true
+        })
+        .add({
+            targets: '.ml3 .letter',
+            opacity: [0, 1],
+            easing: "easeInOutQuad",
+            duration: 2250,
+            delay: (el, i) => 150 * (i + 1)
+        }).add({
+            targets: '.ml3',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+
+
+
+});
+
+function delet(e) {
+    var existingEntries = JSON.parse(localStorage.getItem("allEntries")); //get data 
+    var place = $(e).parent().parent().attr("id"); // getting index of item 
+    existingEntries.splice(place, 1); //removing using splice 
+    localStorage.setItem("allEntries", JSON.stringify(existingEntries)); //storing back to local storage 
+    location.reload(); // reload page 
+
+};
+
+function edit(w) {
+    var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+    var place = $(w).parent().parent().attr("id");
+    $("#submit").addClass("edit")
+    $("#submit").attr("editid", place)
+    for (i = 0; i < existingEntries.length; i++) {
+        if (place == i) {
+            var fname = $("#fname").val(existingEntries[i].title)
+            var lname = $("#lname").val(existingEntries[i].text)
+            var age = $("#Age").val(existingEntries[i].Age)
+            var sex = $("input:radio[name=gender]:checked").val(existingEntries[i].gender)
+        }
+    }
+}
